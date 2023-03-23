a335d9a0f7030b53d1c2edbed6df8c3742be5ebd
iseebee <max10999@mail.ru>
2023-03-24 01:20:49 +0300

    Урок 5

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,11 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+
+with open('my_file.txt') as str_f:
+    f_strs = str_f.readlines()
+    f_strs1 = [el[:-1] for el in f_strs[:-1]]
+    print(f'Кол-во строк: {len(f_strs1)}')
+    for ind, el in enumerate(f_strs1, 1):
+        print(ind, len(el))
--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,15 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+
+
+import random
+
+with open("text5.txt", "w+") as f_str:
+    for i in range(random.randint(1, 10)):
+        f_str.write(str(random.randint(1, 100)) + ' ')
+f_sum = 0
+with open("text5.txt") as f_str:
+    for i in f_str.read().split():
+        f_sum += int(i)
+print(f_sum)
